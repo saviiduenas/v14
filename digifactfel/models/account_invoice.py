8c558bfa2c200e8b9a4b2603dd225376edb64415
Savi Dueñas <savi@noduu.com>
2022-06-08 18:01:28 -0600

    actualizando digifactfel

--- digifactfel/models/account_invoice.py
+++ digifactfel/models/account_invoice.py
@@ -213,7 +213,7 @@ class AccountMove(models.Model):
                     "CorreoEmisor": str(factura.company_id.email) or "",
                     "NITEmisor": str(nit_company),
                     "NombreComercial": factura.journal_id.feel_nombre_comercial,
-                    "NombreEmisor": factura.company_id.name,
+                    "NombreEmisor": factura.company_id.company_registry,
                 }
 
                 if tipo == "FPEQ":
